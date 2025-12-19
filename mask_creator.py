cbcf648f53f508eb886910df2750ec1982d96faf
liyas1719 <lasariaissa@smith.edu>
2025-12-19 19:26:48 +0000

    adding final images, running mask_creator

--- mask_creator.py
+++ mask_creator.py
@@ -3,11 +3,11 @@ import numpy as np
 from PIL import Image
 import numpy as np
 
-image = Image.open('/workspaces/CSC334bd-Final-Project---Midline-Identification/images/image01.jpg').convert('L')
+image = Image.open('/workspaces/Midline-Identification_CSC334bd_Barresi-Lab/images/image43.jpg').convert('L')
 image_array = np.array(image)
 image_array = (image_array > 200).astype(np.uint8) * 255
 
 print(sum(image_array))
 print(image_array.shape)
 
-np.save("image01.npy", image_array)
+np.save("image43.npy", image_array)
